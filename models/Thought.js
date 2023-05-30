97888807a066c1d21ac3e655cb2ce622c694d906
unknown <thomasjjohnson9311@gmail.com>
2023-05-30 00:07:34 -0500

    changed all functions to match

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require("mongoose");
-// was Application
+// was Thought
 const Reaction = require("./Reaction");
 
 // Schema to create Post model
@@ -32,15 +32,15 @@ const thoughtSchema = new Schema(
   }
 );
 
-// Create a virtual property `getTags` that gets the amount of tags associated with an application
-applicationSchema
+// Create a virtual property `getTags` that gets the amount of tags associated with an thought
+thoughtSchema
   .virtual("getResponses")
   // Getter
   .get(function () {
     return this.tags.length;
   });
 
-// Initialize our Application model
+// Initialize our Thought model
 const Thought = model("thought", thoughtSchema);
 
 module.exports = Thought;
