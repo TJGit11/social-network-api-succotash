0c0f4bd71d053de60f514deca2eb8fced6c79516
unknown <thomasjjohnson9311@gmail.com>
2023-06-01 23:12:56 -0500

    created addfriend function

--- controllers/userController.js
+++ controllers/userController.js
@@ -53,6 +53,24 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  async addFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        {
+          $addToSet: { friends: req.body },
+        },
+        { runValidators: true, new: true }
+      );
+      if (!user) {
+        return res.status(404).json({ message: "No user with this id!" });
+      }
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
   // Delete a user and associated apps
   async deleteUser(req, res) {
     try {
